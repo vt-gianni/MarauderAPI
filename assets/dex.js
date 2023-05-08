e9bd9dfa8d7363e2be989e42799dc0fe5833901b
Gianni GIUDICE <gg@vitalytech.com>
2023-05-08 19:28:47 +0200

    :sparkles: Recherche dans le dex + see more + :construct: :bento: Récupération d'images en cours

--- assets/dex.js
+++ assets/dex.js
@@ -10,3 +10,78 @@ import './styles/dex.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+let offset = 20;
+const charactersContainer = document.querySelector('.characters');
+const defaultBackground = 'https://www.serieously.com/app/uploads/2021/07/harry-potter-choixpeau-magique-poudlard.jpg';
+
+const addHoverStyles = (element, styles) => {
+    element.addEventListener('mouseover', () => {
+        Object.assign(element.style, styles);
+    });
+
+    element.addEventListener('mouseout', () => {
+        Object.keys(styles).forEach(styleKey => {
+            element.style[styleKey] = '';
+        });
+    });
+}
+
+document.getElementById('see-more').addEventListener('click', async () => {
+    const response = await fetch(`/dex/load-more/${offset}`);
+    const characters = await response.json();
+
+    characters.forEach(character => {
+        const characterItem = document.createElement('div');
+        characterItem.classList.add('character-item');
+        characterItem.id = `character-${character.id}`;
+
+        const characterPicture = document.createElement('div');
+        characterPicture.classList.add('character-picture');
+        characterPicture.style.backgroundImage = `url('${character.picture ? character.picture : defaultBackground}')`;
+        characterPicture.style.backgroundSize = 'cover';
+        characterPicture.style.backgroundRepeat = 'no-repeat';
+        characterPicture.style.backgroundPosition = 'center center';
+
+        const characterInsight = document.createElement('div');
+        characterInsight.classList.add('character-insight');
+
+        const characterName = document.createElement('p');
+        characterName.classList.add('character-name', 'badge', `badge-${character.house ? character.house : 'None'}`);
+        characterName.textContent = `${character.firstName} ${character.lastName ? character.lastName : ''}`;
+
+        characterInsight.appendChild(characterName);
+        characterItem.appendChild(characterPicture);
+        characterItem.appendChild(characterInsight);
+        charactersContainer.appendChild(characterItem);
+
+        const pictureDiv = document.createElement('div');
+        pictureDiv.style.backgroundImage = `url('${character.picture ? character.picture : defaultBackground}')`;
+        characterPicture.appendChild(pictureDiv);
+
+        // Ajoutez les styles en hover
+        addHoverStyles(pictureDiv, {
+            transform: 'scale(1.2)',
+            filter: 'brightness(0.5)',
+        });
+    });
+
+    offset += 20;
+})
+
+const searchbar = document.getElementById('searchbar');
+searchbar.addEventListener('input', () => {
+    const searchTerm = searchbar.value.toLowerCase();
+    const characterItems = document.querySelectorAll('.character-item');
+
+    characterItems.forEach(item => {
+        const nameBadge = item.querySelector('.character-name');
+        const name = nameBadge.textContent.toLowerCase();
+
+        if (name.includes(searchTerm)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
--- src/Controller/DexController.php
+++ src/Controller/DexController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Character;
 use App\Repository\CharacterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,5 +30,27 @@ class DexController extends AbstractController
         return $this->render('dex/index.html.twig', [
             'characters' => $this->characterRepository->findBy([], ['id' => 'ASC'], 20)
         ]);
+    }
+
+    /**
+     * @Route("/load-more/{offset}")
+     * @param int $offset
+     * @return JsonResponse
+     */
+    public function loadMore(int $offset): JsonResponse
+    {
+        $characters = $this->characterRepository->findBy([], null, 20, $offset);
+
+        $response = [];
+        foreach ($characters as $character) {
+            $response[] = [
+                'firstName' => $character->getFirstName(),
+                'lastName' => $character->getLastName(),
+                'house' => $character->getHouse(),
+                'picture' => $character->getPicture(),
+            ];
+        }
+
+        return new JsonResponse($response);
     }
 }
